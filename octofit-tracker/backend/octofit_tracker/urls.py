7b0b0a12c80ee6cdaea277d73f03de5def721fd0
Phani <kbphani99@gmail.com>
2025-04-08 17:28:07 +0000

    Commit all changes

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -1,34 +1,11 @@
-<<<<<<< HEAD
 """
 URL configuration for octofit_tracker project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-=======
-"""octofit_tracker URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
->>>>>>> 29ba811 (Add initial backend structure for OctoFit Tracker)
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
 from django.contrib import admin
-<<<<<<< HEAD
-from django.urls import path
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-=======
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, TeamViewSet, ActivityViewSet, LeaderboardViewSet, WorkoutViewSet, api_root
@@ -44,5 +21,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', api_root, name='api-root'),
     path('api/', include(router.urls)),
->>>>>>> 29ba811 (Add initial backend structure for OctoFit Tracker)
 ]
